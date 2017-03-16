d5f06efae78c1b37c295543ba54facc21e3b65ee
Ash Ketchup <ashketchup@sbcglobal.net>
2017-03-15 21:43:10 -0500

    formatted clock

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
+    var dateTime = new Date();
     this.state = {
-      dateTime: new Date()
+      hours: dateTime.getHours(),
+      minutes: dateTime.getMinutes(),
+      seconds: dateTime.getSeconds()
     };
   }
 
@@ -16,15 +19,43 @@ class App extends Component {
   }
 
   updateDateTime() {
+    var dateTime = new Date();
+    var hour = dateTime.getHours();
+    var minute = dateTime.getMinutes().toLocaleString("US",{minimumIntegerDigits:2});
+    var second = dateTime.getSeconds().toLocaleString("US",{minimumIntegerDigits:2});
+//    var addZero = function(digit){
+  //    var num = "0" + digit.toString();
+    //  return num;
+    //}
+    //if(second<10){
+      //second = addZero(second);
+    //}
+    var ampm;
+    if(hour>11){
+      ampm=" PM";
+    }
+    else {
+      ampm=" AM";
+    }
+    if(hour>11){
+      hour-=12;
+    }
     this.setState({
-      dateTime: new Date()
+      hours: hour,
+      minutes: minute,
+      seconds: second,
+      ampm1: ampm
     });
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.dateTime.toString()}
+        <span>{this.state.hours}:</span>
+        <span>{this.state.minutes}:</span>
+        <span>{this.state.seconds}</span>
+        <span>{this.state.ampm1}</span>
+
       </div>
     );
   }
